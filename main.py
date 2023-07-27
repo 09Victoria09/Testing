a07f14ebe2bdb93537d6c7ee4f608d962ecdbb05
viktoriiabaghru <23518559-viktoriiabaghru@users.noreply.replit.com>
2023-07-27 19:08:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+text=input("Введите тескт:")
+first_sym=input("Введите первую букву:")
+second_sym=input("Введите вторую букву:")
+firstSymCount=0
+secondSymCount=0
+
+for symbol in text:
+  if symbol==first_sym:
+    firstSymCount+=1
+  if symbol==second_sym:
+    secondSymCount+=1
+
+print("Кол-во букв",first_sym,"=",firstSymCount)
+print("Кол-во букв",second_sym,"=",secondSymCount)
